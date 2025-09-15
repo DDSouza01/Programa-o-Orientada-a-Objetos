5bf10edc9be7d5cbdf18ec44c13cc8cc8c68e70e
DDSouza01 <danilo_nilo91@hotmail.com>
2025-09-14 22:05:55 -0300

    Update questao02.py

--- Unidade02/Questão02/questao02.py
+++ Unidade02/Questão02/questao02.py
@@ -6,10 +6,9 @@ class Livro:
         self.ano_publicacao = ano_publicacao
         self.preco = preco
 
-
 livro1 = Livro("Arte de projetar em arquitetura", "Ernst Neufert", 2022, 237.30)
 livro2 = Livro("Arquitetura popular brasileira", "Gunter Weimer", 2012, 67.43)
 
 print(f"Título: {livro1.titulo}, Autor: {livro1.autor}, Ano: {livro1.ano_publicacao}, Preço: R${livro1.preco}")
-
 print(f"Título: {livro2.titulo}, Autor: {livro2.autor}, Ano: {livro2.ano_publicacao}, Preço: R${livro2.preco}")
+

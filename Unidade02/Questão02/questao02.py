8e16ffefd0bc14921d8aaffa1aa696b29355a930
DDSouza01 <danilo_nilo91@hotmail.com>
2025-09-14 21:59:08 -0300

    Update and rename Unidade02/questao02.py to Unidade02/Questão02/questao02.py

--- Unidade02/Questão02/questao02.py
+++ Unidade02/Questão02/questao02.py
@@ -1,16 +1,14 @@
-# Definição da classe Livro
+
 class Livro:
-    # Método construtor (__init__)
+    
     def __init__(self, titulo, autor, ano_publicacao, preco):
         self.titulo = titulo
         self.autor = autor
         self.ano_publicacao = ano_publicacao
         self.preco = preco
 
-# Criando dois objetos da classe Livro
 livro1 = Livro("Arte de projetar em arquitetura", "Ernst Neufert", 2022, 237.30)
 livro2 = Livro("Arquitetura popular brasileira", "Gunter Weimer", 2012, 67.43)
 
-# Exibindo informações formatadas
 print(f"Título: {livro1.titulo}, Autor: {livro1.autor}, Ano: {livro1.ano_publicacao}, Preço: R${livro1.preco}")
 print(f"Título: {livro2.titulo}, Autor: {livro2.autor}, Ano: {livro2.ano_publicacao}, Preço: R${livro2.preco}")

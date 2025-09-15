5a037455ed32210cab6c192067eb2ba88b0e3d45
DDSouza01 <danilo_nilo91@hotmail.com>
2025-09-14 22:03:13 -0300

    Update and rename Unidade02/questao02.py to Unidade02/Questão02/questao02.py

--- Unidade02/Questão02/questao02.py
+++ Unidade02/Questão02/questao02.py
@@ -11,4 +11,5 @@ livro1 = Livro("Arte de projetar em arquitetura", "Ernst Neufert", 2022, 237.30)
 livro2 = Livro("Arquitetura popular brasileira", "Gunter Weimer", 2012, 67.43)
 
 print(f"Título: {livro1.titulo}, Autor: {livro1.autor}, Ano: {livro1.ano_publicacao}, Preço: R${livro1.preco}")
+
 print(f"Título: {livro2.titulo}, Autor: {livro2.autor}, Ano: {livro2.ano_publicacao}, Preço: R${livro2.preco}")
